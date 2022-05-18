70d136c31bde911a2d4cff3f7abd878bf8ea6a8d
Davyd Polianchuk <dpolyanchuk75@gmail.com>
2022-05-19 00:38:26 +0200

    RegistrationTest

--- webdriverjsTest.js
+++ webdriverjsTest.js
@@ -8,38 +8,33 @@ until = wd.until;
 var desiredCaps = { 
     platformName: "Android", 
     deviceName: "emulator-5554", 
-    app: "", 
-    appPackage: "com.android.calculator2", 
-    appActivity: ".Calculator", 
-    browserName: '',
+    browserName: "Chrome",
 };
 
 
-async function testCalculator() { 
+async function testPaymi() { 
     //Initiating the Driver 
     let driver = await new wd.Builder().usingServer("http://127.0.0.1:4723/wd/hub").withCapabilities(desiredCaps).build(); 
+    driver.get("https://staging.paymi.com/users/sign_in");
 
     // Automation command. 
-    // 
-    let el4 = await driver.findElement(By.xpath("//android.widget.LinearLayout[@content-desc='Numbers and basic operations']/android.view.ViewGroup[1]/android.widget.Button[1]"));
-    await el4.click();
-    await el4.click();
-    let el5 = await driver.findElement(By.xpath("//android.widget.Button[@content-desc='delete']"));
-    await el5.click();
-    let el6 = await driver.findElement(By.xpath("//android.widget.Button[@content-desc='plus']"));
-    await el6.click();
-    let el7 = await driver.findElement(By.xpath("//android.widget.LinearLayout[@content-desc='Numbers and basic operations']/android.view.ViewGroup[1]/android.widget.Button[9]"));
-    await el7.click();
-    let el8 = await driver.findElement(By.xpath("//android.widget.Button[@content-desc='equals']"));
-    await el8.click();
-    let el10 = await driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='More options']"));
-    await el10.click();
-    let el11 = await driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.TextView"));
-    await el11.click();
-    let el12 = await driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"));
-    await el12.click();
+    const CreateAnAccountTextLink = await driver.findElement(By.xpath("//form[@id='new_user']/div[3]/div[2]/div[2]/a")); 
+    await CreateAnAccountTextLink.click(); 
+    driver.get("https://staging.paymi.com/users/sign_up");
+    const signupEmailInputField = await driver.findElement(By.id("user_email")); 
+    await signupEmailInputField.click(); 
+    await signupEmailInputField.sendKeys("testmailDavydPolianchuk@gmail.com"); 
+    const signupPasswordInputField = await driver.findElement(By.id("user_password")); 
+    await signupPasswordInputField.sendKeys("DavydPolianchuk_123"); 
+    const TermsCheckbox = await driver.findElement(By.id("terms-label"));
+    await TermsCheckbox.click(); 
+    const OffersCheckbox = await driver.findElement(By.xpath("//form[@id='new_user']/div[5]/label"));
+    await OffersCheckbox.click(); 
+    const SignUpButton = await driver.findElement(By.xpath("//input[@name='commit']"));
+    await SignUpButton.click(); 
 
-    await driver.quit();
-}
+    await driver.quit(); 
+}	
+
 //Call the Function
-testCalculator()
+testPaymi()
